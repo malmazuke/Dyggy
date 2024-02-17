7753ef06746c46f8537f57aa88bb28bc948351b5
Mark Feaver <mark.feaver@gmail.com>
2024-02-17 12:01:56 +1100

    Initialised Xcode proj

---
+++ Dyggy/DyggyApp.swift
@@ -0,0 +1,32 @@
+//
+//  DyggyApp.swift
+//  Dyggy
+//
+//  Created by Mark Feaver on 17/2/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DyggyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

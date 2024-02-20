31f196edaaaa653d9b1477a04033210bed50e0ef
Mark Feaver <mark.feaver@gmail.com>
2024-02-20 12:18:02 +1100

    Add vendor and product IDs to Dygma devices

--- Focus-API-MacOS/Sources/API/FocusAPI.swift
+++ Focus-API-MacOS/Sources/API/FocusAPI.swift
@@ -1,25 +1,59 @@
 public enum DygmaDevice {
     case defyWired
     case defyWireless
-    case raiseWired
-    case raiseWireless
+    case raiseANSI
+    case raiseISO
+    
+    var vendorId: Int {
+        switch self {
+        case .defyWired:
+            0x35ef
+        case .defyWireless:
+            0x35ef
+        case .raiseANSI:
+            0x1209
+        case .raiseISO:
+            0x1209
+        }
+    }
+    
+    var productId: Int {
+        switch self {
+        case .defyWired:
+            0x0010
+        case .defyWireless:
+            0x0012
+        case .raiseANSI:
+            0x2200
+        case .raiseISO:
+            0x2201
+        }
+    }
+    
+    static var allDevices: Set<DygmaDevice> {
+        [.defyWired, .defyWireless, .raiseANSI, .raiseISO]
+    }
+
 }
 
-public struct FoundDygmaDevice: Hashable {
+public struct ConnectedDygmaDevice: Hashable {
+
     // TODO: Add relevant information, ports, etc
-    var id: String
+    var vendorId: Int
+    var productId: Int
+
 }
 
 public protocol FocusAPI {
 
-    func find(devices: Set<DygmaDevice>) async throws -> Set<FoundDygmaDevice>
+    func find(devices: Set<DygmaDevice>) async throws -> Set<ConnectedDygmaDevice>
 
 }
 
 public class DefaultFocusAPI: FocusAPI {
 
-    public func find(devices: Set<DygmaDevice>) async throws -> Set<FoundDygmaDevice> {
-        return Set<FoundDygmaDevice>()
+    public func find(devices: Set<DygmaDevice>) async throws -> Set<ConnectedDygmaDevice> {
+        return Set<ConnectedDygmaDevice>()
     }
 
 }
--- Focus-API-MacOS/Tests/DefaultFocusAPITests.swift
+++ Focus-API-MacOS/Tests/DefaultFocusAPITests.swift
@@ -2,30 +2,24 @@ import XCTest
 @testable import Focus_API_MacOS
 
 final class DefaultFocusAPITests: XCTestCase {
-
-    var testSubject: DefaultFocusAPI!
     
+    var testSubject: DefaultFocusAPI!
+
     override func setUp() async throws {
         testSubject = DefaultFocusAPI()
     }
-    
-    func testFindDevices() async throws {
-        // GIVEN devices are connected
-        
+
+    func testFindAllDevices() async throws {
+        // GIVEN all devices are connected
+
         // AND I expect to find four connected devices
-        let expectedDevices: Set<FoundDygmaDevice> = [
-            FoundDygmaDevice(id: "1"),
-            FoundDygmaDevice(id: "2"),
-            FoundDygmaDevice(id: "3"),
-            FoundDygmaDevice(id: "4"),
-        ]
-        
+        let expectedDevices: Set<ConnectedDygmaDevice> = Set(DygmaDevice.allDevices.map { ConnectedDygmaDevice(vendorId: $0.vendorId, productId: $0.productId) })
+
         // WHEN Focus API attempts to find devices
-        let foundDevices = try await testSubject.find(devices: [.defyWired, .defyWireless, .raiseWired, .raiseWireless])
-        
+        let foundDevices = try await testSubject.find(devices: DygmaDevice.allDevices)
+
         // THEN Devices are found
         XCTAssertEqual(expectedDevices, foundDevices)
-        
     }
-    
+
 }
